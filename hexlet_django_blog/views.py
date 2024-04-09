39f6120834b46972d96d4092fd2a6756c0299d48
Asgef <asgefes1@gmail.com>
2024-04-09 16:30:40 +0300

    Add template about.html

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 
+TEAM = [
+    {'name': 'Yoda', 'position': 'CEO'},
+    {'name': 'Obi-Wan Kenobi', 'position': 'Senior Developer'},
+    {'name': 'Anakin Skywalker', 'position': 'Junior Developer'},
+    {'name': 'Jar Jar Binks', 'position': 'Trainee'},
+]
+
+
 def index(request):
     return render(request, 'index.html', context={
         'who': 'World',
@@ -12,5 +20,5 @@ def about(request):
     return render(
         request,
         'about.html',
-        context={'tags': tags},
+        context={'tags': tags, 'TEAM': TEAM},
     )
